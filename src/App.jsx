ddfa130c507f805e61a03ae7e2a67295c4b2f069
Alexander Linse <zandervon24@gmail.com>
2024-03-29 15:31:37 -0500

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h2>TODO: Add a username/password sign-in form, that gets and authenticates a JWT</h2>
+    </>
+  )
+}
+
+export default App
