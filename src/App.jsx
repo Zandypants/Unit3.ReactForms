3226cc1ce04d356999592222b8948f285caa7332
Alexander Linse <zandervon24@gmail.com>
2024-03-30 14:07:36 -0500

    Added Sign Up form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import SignUpForm from './SignUpForm'
+import Authenticate from './Authenticate'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <SignUpForm></SignUpForm>
+      <Authenticate></Authenticate>
     </>
   )
 }
--- src/SignUpForm.jsx
+++ src/SignUpForm.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react"
+
 const SignUpForm = () => {
-  return <h2>Sign Up</h2>
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    console.log("Form submit pressed! TODO: functionality")
+  }
+
+  return <section>
+    <h2>Sign Up</h2>
+    <form className="columnContainer"  onSubmit={onSubmit}>
+      <div className="inputLine">
+        <label htmlFor="username">UserName: </label>
+        <input type="text" name="username" onChange={(event)=>setUserName(event.target.value)}></input>
+      </div>
+      <div className="inputLine">
+        <label htmlFor="password">Password: </label>
+        <input type="password" name="password" onChange={(event)=>setPassword(event.target.value)}></input>
+      </div>
+      <input type="submit"></input>
+    </form>
+  </section>
 }
 
 export default SignUpForm
